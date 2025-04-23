f36a02f26e8ce6ae8baae8542b27624560792259
M Wahhab Mirza - Vector Labs FZE LLC <144521746+MWahhabMirzaVectorLabsFZELLC@users.noreply.github.com>
2025-04-23 17:21:34 +0500

    Delete src directory

--- src/components/trade-page/ablyClient.ts
+++
@@ -1,9 +0,0 @@
-// ablyClient.ts
-import Ably from "ably";
-
-const ably = new Ably.Realtime("qR94EA.6wk1Hw:8rEGkjC032riq48eMyuO0sHv4K_j04YjhZGpoYF1hCU");
-
-// Get the shared channel – reuse this everywhere
-const channel = ably.channels.get("coins");
-
-export { ably, channel };
